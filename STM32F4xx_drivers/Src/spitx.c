83ad37a059c163733f0427a90a4c17b44658c26d
Eashan Gupta <eashanvgupta@gmail.com>
2025-09-08 22:49:12 -0500

    changes to spitx and added csproj

--- STM32F4xx_drivers/Src/spitx.c
+++ STM32F4xx_drivers/Src/spitx.c
@@ -8,6 +8,13 @@
 #include "stm32f411xx.h"
 #include "stm32f411xx_spi_driver.h"
 
+// SPI2 pins
+// PB15 -- MOSI
+// PB14 -- MISO
+// PB13 -- SCK
+// PB12 -- NSS
+// Alt function mode : 5
+
 int main(void)
 {
 	return 0;
